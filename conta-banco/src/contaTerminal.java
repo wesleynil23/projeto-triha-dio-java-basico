1fca111678d630fb3d005b629a63bfacdd20f87a
wesley nilson <wesleysales2358@gmail.com>
2024-07-18 15:10:31 -0300

    projeto finalizado

--- conta-banco/src/contaTerminal.java
+++ conta-banco/src/contaTerminal.java
@@ -1,5 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class contaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        // exibir as mensagens para o usuário
+        // obter valor digitado pelo usuário
+
+        System.out.println("Por favor, digite o número do seu banco");
+        int Banco = scanner.nextInt();
+
+        System.out.println("Por favor, digite o número da sua agéncia");
+        double Agencia = scanner.nextInt();
+
+        System.out.println("Por favor, digite seu nome");
+        String Nome = scanner.next();
+
+        // exibir mensagem conta criada
+
+        System.out.println("Olá " + Nome + "!");
+        System.out.println("Obrigado por criar uma conta em nosso banco, sua agência é " + Banco + "");
+        System.out.println("Sua conta é " + Agencia + "!");
+        System.out.println("Seu saldo é " + 100 + " já está disponível para saque. Obrigado pela preferencia e tenha um ótimo dia!");
+
     }
 }
